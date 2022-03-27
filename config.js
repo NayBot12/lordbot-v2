0daa63499e3b420b4c20e899490b14987887a771
ryoromance <100901480+NayBot12@users.noreply.github.com>
2022-03-27 09:35:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Copyright Riyo-Botz'
-global.author = 'Riyo Botz Best Bot\nBattry: true\nCopyright By © RiyoOfc '
+global.author = 'Riyo Botz Best Bot\nBattry: false\nCopyright By © RiyoOfc '
 
 global.multiplier = 39 // The higher, The harder levelup
 
